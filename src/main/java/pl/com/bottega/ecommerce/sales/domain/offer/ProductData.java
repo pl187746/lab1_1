70e71d77b6fd973097ceaea435a1a955596a71d7
Karol Kucharski <187746@edu.p.lodz.pl>
2016-03-17 17:05:09 +0100

    Wygenerowanie konstruktora ProductData inicjalizujacego pola

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ProductData.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ProductData.java
@@ -4,6 +4,16 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 public class ProductData {
+	public ProductData(String productId, BigDecimal productPrice, String productName, Date productSnapshotDate,
+			String productType) {
+		super();
+		this.productId = productId;
+		this.productPrice = productPrice;
+		this.productName = productName;
+		this.productSnapshotDate = productSnapshotDate;
+		this.productType = productType;
+	}
+
 	public String productId;
 	public BigDecimal productPrice;
 	public String productName;
