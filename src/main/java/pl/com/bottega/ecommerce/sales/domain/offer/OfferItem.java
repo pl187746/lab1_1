0f88a44e9db68eba2ad7b5898bedb32a6b5ca871
Karol Kucharski <187746@edu.p.lodz.pl>
2016-03-17 18:14:32 +0100

    Wydzielenie metody OfferItem.equalsProductAndQuantity z czesci wspolnej metod OfferItem.equals i OfferItem.sameAs

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
@@ -117,18 +117,26 @@ public class OfferItem {
 				return false;
 		} else if (!discountData.discount.equals(other.discountData.discount))
 			return false;
-		if (productData == null) {
-			if (other.productData != null)
+		if(!equalsProductAndQuantity(other))
 				return false;
-		} else if (!productData.equals(other.productData))
-			return false;
-		if (quantity != other.quantity)
-			return false;
 		if (totalCost == null) {
 			if (other.totalCost != null)
 				return false;
 		} else if (!totalCost.equals(other.totalCost))
 			return false;
+		return true;
+	}
+
+	private boolean equalsProductAndQuantity(OfferItem other) {
+		if (productData == null) {
+			if (other.productData != null)
+				return false;
+		} else if (!productData.equals(other.productData))
+			return false;
+		
+		if (quantity != other.quantity)
+			return false;
+		
 		return true;
 	}
 
@@ -140,13 +148,7 @@ public class OfferItem {
 	 * @return
 	 */
 	public boolean sameAs(OfferItem other, double delta) {
-		if (productData == null) {
-			if (other.productData != null)
-				return false;
-		} else if (!productData.equals(other.productData))
-			return false;
-
-		if (quantity != other.quantity)
+		if(!equalsProductAndQuantity(other))
 			return false;
 
 		BigDecimal max, min;
