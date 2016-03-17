57e81925beb06f9b67405bc8a9ac876ae9692672
Karol Kucharski <187746@edu.p.lodz.pl>
2016-03-17 18:05:58 +0100

    Wygenerowanie equals i hashCode w DiscountData

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/DiscountData.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/DiscountData.java
@@ -10,4 +10,35 @@ public class DiscountData {
 		this.discount = discount;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((discount == null) ? 0 : discount.hashCode());
+		result = prime * result + ((discountCause == null) ? 0 : discountCause.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DiscountData other = (DiscountData) obj;
+		if (discount == null) {
+			if (other.discount != null)
+				return false;
+		} else if (!discount.equals(other.discount))
+			return false;
+		if (discountCause == null) {
+			if (other.discountCause != null)
+				return false;
+		} else if (!discountCause.equals(other.discountCause))
+			return false;
+		return true;
+	}
+
 }
