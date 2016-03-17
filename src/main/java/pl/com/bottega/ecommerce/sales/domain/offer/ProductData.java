91f266738547c9588628437ea17e1c396be7dfe7
Karol Kucharski <187746@edu.p.lodz.pl>
2016-03-17 17:15:13 +0100

    Wygenerowanie metod equals i hashCode w ProductData

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ProductData.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ProductData.java
@@ -19,5 +19,46 @@ public class ProductData {
 	public String productName;
 	public Date productSnapshotDate;
 	public String productType;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
+		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+		result = prime * result + ((productPrice == null) ? 0 : productPrice.hashCode());
+		result = prime * result + ((productType == null) ? 0 : productType.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductData other = (ProductData) obj;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		if (productName == null) {
+			if (other.productName != null)
+				return false;
+		} else if (!productName.equals(other.productName))
+			return false;
+		if (productPrice == null) {
+			if (other.productPrice != null)
+				return false;
+		} else if (!productPrice.equals(other.productPrice))
+			return false;
+		if (productType == null) {
+			if (other.productType != null)
+				return false;
+		} else if (!productType.equals(other.productType))
+			return false;
+		return true;
+	}
 
 }
