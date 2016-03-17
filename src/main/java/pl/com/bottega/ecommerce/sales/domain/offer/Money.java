a1453141e67280951ae8b03143315bcda8dc9742
Karol Kucharski <187746@edu.p.lodz.pl>
2016-03-17 18:01:15 +0100

    Usuniecie konstruktora domyslnego Money

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -6,9 +6,6 @@ public class Money {
 	public BigDecimal value;
 	public String currency;
 
-	public Money() {
-	}
-
 	public Money(BigDecimal value, String currency) {
 		super();
 		this.value = value;
